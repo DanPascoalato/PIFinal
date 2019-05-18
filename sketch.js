04f36d42ead259dcb6a474acbc8d9a7d5f354e28
DanielPasco <daniel.pascoalato@uol.com.br>
2019-05-17 21:53:07 -0300

    Mudança do RK4

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 var quantiA = 0;
 var quantiB = 0;
 var quantiC = 0;
-var timeStep = 1; 
+var timeStep = 0.001; 
 var vazaoA = 0.1;
 var vazaoB = 0.05;
 var vazaoC = 0.01;
@@ -116,54 +116,52 @@ function draw() {
     text('B', 780, 75);
     text('C', 780, 105);
     
-    frameRate(10);
+    frameRate(600);
     //tabela([quantiA, quantiB]);
-    
-    
-    
     //console.log(quantiA, quantiB, quantiC);
-    tabela([quantiA, quantiB, quantiC]);
-    CalculaA();
     checaA = quantiA;
-    quantiA = salvaA;
-    CalculaB();
-    quantiB = salvaB;
-    CalculaC();
-    quantiC = salvaC;
+    
+    tabela([flag,quantiA, quantiB, quantiC]);
+    
+    quantiA = CalculaA(quantiA, quantiC);
+    quantiB = CalculaB(quantiB, quantiA);
+    quantiC = CalculaC(quantiC, quantiB);
     flag++;
+    
     if(flag == dias){
         noLoop();
         console.log(quantiA, quantiB, quantiC);
         text('Valor no final do dia selecionado de cada container: A: ' + quantiA + ', B: ' + quantiB + ', C: ' + quantiC, 50, 350);
-    } else if (quantiA.toFixed(8) == checaA.toFixed(8) && flag != dias){
+    } 
+    else if (quantiA.toFixed(8) == checaA.toFixed(8) && flag != dias){
         noLoop();
         console.log(quantiA, quantiB, quantiC);
         text('O sistema entrou em equilibrio no dia ' + flag + ', a quantidade dos containers é de: A: ' + quantiA + ', B: ' + quantiB + ', C: ' + quantiC, 50, 350);
     }
 }
 
-function CalculaA(){
+function CalculaA(quantiA, quantiC) {
     var k1 = calcKA(quantiA, quantiC);
     var k2 = calcKA(quantiA + (k1 * timeStep / 2), quantiC + (k1 * timeStep / 2));
     var k3 = calcKA(quantiA + (k2 * timeStep / 2), quantiC + (k2 * timeStep / 2));
     var k4 = calcKA(quantiA + (k3 * timeStep), quantiC + (k3 * timeStep));
-    salvaA = quantiA + timeStep * (k1 + 2*k2 + 2*k3 + k4) / 6;
+    return quantiA + timeStep * (k1 + 2*k2 + 2*k3 + k4) / 6;
 }
 
-function CalculaB(){
+function CalculaB(quantiB, quantiA){
     var l1 = calcKB(quantiB, quantiA);
     var l2 = calcKB(quantiB + (l1 * timeStep / 2), quantiA + (l1 * timeStep / 2));
     var l3 = calcKB(quantiB + (l2 * timeStep / 2), quantiA + (l2 * timeStep / 2));
     var l4 = calcKB(quantiB + (l3 * timeStep), quantiA + (l3 * timeStep));
-    salvaB = quantiB + timeStep * (l1 + 2*l2 + 2*l3 + l4) / 6;
+    return quantiB + timeStep * (l1 + 2*l2 + 2*l3 + l4) / 6;
 }
 
-function CalculaC(){
+function CalculaC(quantiC, quantiB){
     var m1 = calcKC(quantiC, quantiB); 
     var m2 = calcKC(quantiC + (m1 * timeStep / 2), quantiB + (m1* timeStep / 2));
     var m3 = calcKC(quantiC + (m2 * timeStep / 2), quantiB + (m2 * timeStep / 2));
     var m4 = calcKC(quantiC + (m3 * timeStep), quantiB + (m3 * timeStep));
-    salvaC = quantiC + timeStep * (m1 + 2*m2 + 2*m3 + m4) / 6;
+    return quantiC + timeStep * (m1 + 2*m2 + 2*m3 + m4) / 6;
     //arrayPontos.push(new Ponto(flag,salvaA));   
 }
 
